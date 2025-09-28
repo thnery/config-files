1e77f45ccb62e9b2a0a51c05607978e1932a58bd
Tácio Nery <tacionery@gmail.com>
2025-09-28 15:52:44 +0100

    Update keymaps.lua

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,6 +2,9 @@
 
 local keymap = vim.keymap.set
 
+-- Leader key
+vim.g.mapleader = ","
+
 -- NERDTree
 keymap('n', '<C-t>', ':NERDTreeToggle<CR>', { desc = 'Toggle NerdTree' })
 
@@ -19,6 +22,12 @@ keymap('n', '<leader>rn', vim.lsp.buf.rename, { desc = 'Rename' })
 keymap('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code action' })
 keymap('n', 'gr', vim.lsp.buf.references, { desc = 'References' })
 
+-- LSP Diagnostics
+keymap('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Show line diagnostics' })
+keymap('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic' })
+keymap('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic' })
+keymap('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
 -- Rails shortcuts
 keymap('n', '<leader>ra', ':A<CR>', { desc = 'Rails alternate file' })
 keymap('n', '<leader>rr', ':R<CR>', { desc = 'Rails related file' })
@@ -30,9 +39,12 @@ keymap('n', '<C-k>', '<C-w>k', { desc = 'Window up' })
 keymap('n', '<C-l>', '<C-w>l', { desc = 'Window right' })
 
 -- Buffer navigation
-keymap('n', '<leader>bn', ':bnext<CR>', { desc = 'Next buffer' })
-keymap('n', '<leader>bp', ':bprevious<CR>', { desc = 'Previous buffer' })
-keymap('n', '<leader>bd', ':bdelete<CR>', { desc = 'Delete buffer' })
+keymap('n', '<C-l>', ':bnext<CR>', { desc = 'Next buffer' })
+keymap('n', '<C-k>', ':bprevious<CR>', { desc = 'Previous buffer' })
+keymap('n', '<C-d>', ':bdelete<CR>', { desc = 'Delete buffer' })
 
 -- Quick save
 keymap('n', '<leader>w', ':w<CR>', { desc = 'Save file' })
+
+-- Clipboard operations
+keymap('v', '<leader>y', '"+y', { desc = 'Copy to system clipboard' })
