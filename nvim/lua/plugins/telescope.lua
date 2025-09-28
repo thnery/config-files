095b7280d316caccfe363960421c13d94d88f283
Tácio Nery <tacionery@gmail.com>
2025-09-28 16:06:45 +0100

    Update telescope.lua

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,19 +1,5 @@
 -- telescope config
 
--- return {
---   "nvim-telescope/telescope.nvim",
---   tag = "0.1.8",
---   dependencies = { "nvim-lua/plenary.nvim" },
---   opts = {
---     defaults = {
---       layout_strategy = "horizontal",
---       layout_config = { prompt_position = "top" },
---       sorting_strategy = "ascending",
---       winbled = 0,
---     },
---   },
--- }
-
 require('telescope').setup({
   defaults = {
     mappings = {
